4a471d196ba1b89a16921f98065822204a4235d6
Jurijs Kolesnikovs <jurijs_kolesnikovs@inbox.lv>
2019-03-01 11:40:16 +0200

    Added custom SCSS files, bootstrap, jquery

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-console.log('Main file here!')
-import './main.scss'
-import logo from './webpack.png'
-const app = document.getElementById('app')
-/* 
-const img = document.createElement('img')
-img.src = logo
-img.className = 'logo'
-
-app.appendChild(img) */
+import './js/common'
+import './scss/main.scss'
